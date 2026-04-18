3c44602552efdf8e8d11d98b99eb0bed4601ba15
Intellectusuz <thequotes1001@gmail.com>
2026-04-18 17:15:07 +0500

    questions.js

---
+++ questions.js
@@ -0,0 +1,9 @@
+const questionsBase = {
+    "Huquqshunoslik": [
+        { id: 1, question: "Savol?", options: ["A", "B", "C", "D"], correct: 0 },
+        { id: 2, question: "Ikkinchi savol?", options: ["A", "B", "C", "D"], correct: 1 }
+    ],
+    "Tarix": [
+        { id: 15, question: "Tarixiy savol?", options: ["A", "B", "C", "D"], correct: 0 }
+    ]
+}; // Энг охирида мана шундай ёпилиши керак
